cc9101169ca5db1a252a593ee0a00c0f422e7892
donutt18 <fulodave8@gmail.com>
2024-09-10 19:16:06 +0800

    Adding the Addition Method

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,11 @@
-public class Calculator{
-    public static void main (String[] args){
+public class Calculator {
+    public static void main(String[] args) {
 
-        int a=2;
-        int b=4;
+        int a = 2;
+        int b = 4;
+
+        int sum = a + b;
+        System.out.println("Addition : " + sum);
 
     }
 }
